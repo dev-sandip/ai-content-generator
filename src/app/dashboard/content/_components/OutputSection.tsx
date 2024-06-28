32c33438c06b56156ec9a0bf7a9bbe13b060c273
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-28 23:07:41 +0545

    chore: Add @toast-ui/react-editor dependency and integrate OutputSection component

--- src/app/dashboard/content/_components/OutputSection.tsx
+++ src/app/dashboard/content/_components/OutputSection.tsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
 const OutputSection = () => {
-  return <div>OutputSection</div>;
+  const editorRef = React.useRef<any>(null);
+  return (
+    <div className="bg-white shadow-lg border rounded-lg">
+      <div className="flex items-center justify-between p-5">
+        <h2 className="font-medium text-lg">Your Result:</h2>
+        <Button className="flex gap-2">
+          <Copy />
+          Copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your result will appear here....."
+        height="600px"
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+      />
+    </div>
+  );
 };
 
 export default OutputSection;
